b5068d8946b96f39af413c92b5a3f2e1277e78f5
rinykia <rinykia@gmail.com>
2016-03-31 22:46:49 +0300

    add photo header and footer

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,29 @@
 # Change to match your CPU core count
-workers 2
+#workers 2
 
 # Min and Max threads per worker
-threads 1, 6
+#threads 1, 6
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "/home/rinykia/applications/new-app/shared"
+#app_dir = File.expand_path("../..", __FILE__)
+#shared_dir = "/home/rinykia/applications/new-app/shared"
 
 # Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
+#rails_env = ENV['RAILS_ENV'] || "production"
+#environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+#bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
-state_path "#{shared_dir}/tmp/pids/puma.state"
-activate_control_app
+#pidfile "#{shared_dir}/tmp/pids/puma.pid"
+#state_path "#{shared_dir}/tmp/pids/puma.state"
+#activate_control_app
 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
+#on_worker_boot do
+ # require "active_record"
+ # ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+ # ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+#end
